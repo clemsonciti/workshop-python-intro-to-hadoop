1a9f077bef8384ce41ba2e8f8eb63305d24f9a78
Linh Ngo <lngo@clemson.edu>
2016-10-05 22:01:00 -0400

    add python files under scripts

--- scripts/mapper02.py
+++ scripts/mapper02.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python          
-import os
 import sys            
 import csv                                                       
-movieFile = os.path.join(os.path.dirname(sys.argv[0]),"movies.dat")
+movieFile = "movies.csv"
 movieList = {}
 with open(movieFile, mode = 'r') as infile:
-  for line in infile:
-    row = (line.strip()).split("::")
+  reader = csv.reader(infile)
+  for row in reader:  
     movieList[row[0]] = {}  
     movieList[row[0]]["title"] = row[1].strip()  
     movieList[row[0]]["genre"] = row[2].strip()                       
 for oneMovie in sys.stdin:                                     
   oneMovie = oneMovie.strip()      
-  ratingInfo = oneMovie.split("::")               
+  ratingInfo = oneMovie.split(",")               
   try:
     movieTitle = movieList[ratingInfo[1]]["title"]   
     rating = float(ratingInfo[2])                     
